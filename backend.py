a7a3faa6783b830d7b69e9837685fd00c8af2d2f
Mustafa Umut Kaymaz <mustafaumutkaymaz@etu.edu.tr>
2025-12-02 17:50:00 +0300

    Final commit - Revised 5

--- backend.py
+++ backend.py
@@ -26,8 +26,9 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL, echo=True)
 app = FastAPI()
